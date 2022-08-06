9984ed479ae53348ea3b8b19f53045d6462e86f1
totoftrs <c.guillaume027@gmail.com>
2022-08-06 02:19:54 +0200

    finish

--- shirt-store.js
+++ shirt-store.js
@@ -1,61 +1,121 @@
-export const cost = (shirts) => {
-  // throw new Error("Please remove this statement and implement this function");
+// export const cost = (shirts) => {
   
-    const price = 800
-    let result = 0
-    let map = {}
+//     const price = 800
+//     let result = 0
+//     let map = {}
     
-    if(shirts.length === 0) {
-      return 0 
-    }
+//     if(shirts.length === 0) {
+//       return 0 
+//     }
   
-    for (const number of shirts) {
-      if(map[number]) {
-        map[number] +=1
-      } else {
-        map[number] = 1
-      }
-    }
+//     for (const number of shirts) {
+//       if(map[number]) {
+//         map[number] +=1
+//       } else {
+//         map[number] = 1
+//       }
+//     }
   
-    for (const key in map) {
-     result += map[key] * price
-    }
+//     for (const key in map) {
+//      result += map[key] * price
+//     }
   
-    if(result === 6400) {
-      return result + (result * -20 / 100)
-    }
+//     if(result === 6400) {
+//       return result + (result * -20 / 100)
+//     }
 
-  const CheckLengthObject = Object.keys(map).length
+//   const CheckLengthObject = Object.keys(map).length
 
-  switch (CheckLengthObject) {
-    case 2:
-    return result + (result * -5 / 100);
+//   switch (CheckLengthObject) {
+//     case 2:
+//     return result + (result * -5 / 100);
 
-    case 3:
-    return result + (result * -10 / 100);
+//     case 3:
+//     return result + (result * -10 / 100);
 
-    case 4:
-    return result + (result * -20 / 100);
+//     case 4:
+//     return result + (result * -20 / 100);
 
-    case 5:
-    return result + (result * -25 / 100)
+//     case 5:
+//     return result + (result * -25 / 100)
  
-    default:
-      break;
+//     default:
+//       break;
+//   }
+
+//   return result
+//   }
+
+//SOLUTION --------
+
+const SHIRT_PRICE = 800;
+const DISCOUNTS = [1, 0.95, 0.9, 0.8, 0.75];
+
+// cost of the group of <size> distinct products
+const groupCost = (size) => size * SHIRT_PRICE * DISCOUNTS[size - 1];
+
+// return an object : key is the id of the shirt and value is the number of shirt of this type
+// [1,1,2,3] => {'1':2, '2':1, '3':1}
+const groupShirts = (shirts) => {
+  const counter = {};
+  shirts.forEach((shirt) => {
+    if (!counter[shirt.toString()]) {
+      counter[shirt.toString()] = 1;
+    } else {
+      counter[shirt.toString()]++;
+    }
+  });
+  return counter;
+};
+
+// remove 1 shirt of the <size> most frequent shirt type
+// ex : ({'1':5, '2':4, '3':2},2) => {'1':4, '2':3, '3':2}
+const removeMostFrequent = (volumes, size) => {
+  const res = {};
+  for (let key in volumes) {
+    res[key] = volumes[key];
+  }
+  Object.entries(volumes)
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, size)
+    .forEach((k) => {
+      res[k[0]] = volumes[k[0]] - 1;
+      if (res[k[0]] <= 0) delete res[k[0]];
+    });
+  return res;
+};
+
+// inverse method of groupShirts
+// {'1':2, '2':1, '3':1} => [1,1,2,3]
+const collectionFromGroup = (volumes) => {
+  const subGroup = [];
+  Object.entries(volumes).forEach((entry) => {
+    if (entry[1] > 0) {
+      const sub = new Array(entry[1]);
+      subGroup.push(...sub.fill(parseInt(entry[0])));
+    }
+  });
+  return subGroup;
+};
+
+const calculate = (sortedShirts) => {
+  if (sortedShirts.length === 0) return 0;
+  const volumes = groupShirts(sortedShirts);
+  let price = sortedShirts.length * SHIRT_PRICE;
+  for (let size = Object.keys(volumes).length; size > 1; size--) {
+    const subGroup = removeMostFrequent(volumes, size);
+    const collection = collectionFromGroup(subGroup);
+    price = Math.min(price, groupCost(size) + calculate(collection));
+  }
+  return price;
+};
+
+export const cost = (shirts) => {
+  if (shirts.length === 0) {
+    return 0;
   }
 
-  return result
-  }
-    
+  return calculate(shirts.sort((a, b) => a - b));
+};
 
-/*
-  1 t-shirt tt seul coute 8$
-  2 t-shirt different te donnne une reduction de 5%
-  3 t-shirt different te donnne une reduction de 10%
-  4 t-shirt different te donnne une reduction de 20%
-    4 t-shirt dont 3 sont different 
-    tu as une reduction sur les 3 de 10% et le 4eme coute 8$
-  5 meme t-shirt te donnne une reduction de 25%
-
-*/
 
